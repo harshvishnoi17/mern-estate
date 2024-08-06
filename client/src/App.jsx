41429caca3f1ccaea47069a7ddc3b5727f0c19e7
harsh <vishnoiharsh17@gmail.com>
2024-08-06 09:27:28 -0700

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from './pages/SignUp';
 import Profile from "./pages/Profile";
-import About from './pages/About';
+import About from "./pages/About";
 import Header from "./components/Header";
 
 
