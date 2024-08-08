12cca01c9333216949b7060e412b4651e87c8b50
harsh <vishnoiharsh17@gmail.com>
2024-08-08 08:35:21 -0700

    connect to database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from './pages/SignUp';
+import About from './pages/About';
 import Profile from "./pages/Profile";
-import About from "./pages/About";
 import Header from "./components/Header";
 
 
